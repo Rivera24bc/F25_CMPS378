00413aaf4cb1b92a0f5865b89f964556e0421ccf
Rivera24bc <riveraoscar2929@gmail.com>
2025-09-10 21:52:31 -0700

    Add files via upload

---
+++ Group Exercise #1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string gasStation = "";
+            double tax = 0.0, total = 0.0, price = 0.0, gallons = 0.0, subtotal = 0.0, taxpercent = 0.0;
+
+            Console.Write("Enter the name of the Gas Station: ");
+            gasStation = Console.ReadLine();
+
+            Console.Write("Enter the price of gas: ");
+            price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("How many Gallons: ");
+            gallons = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What is the Tax Percentage for this transaction: ");
+            taxpercent = Convert.ToDouble(Console.ReadLine());
+
+            // Correct calculations
+            subtotal = price * gallons;
+            tax = subtotal * (taxpercent / 100);
+            total = subtotal + tax;
+
+            // Display output
+            Console.WriteLine("*******(" + gasStation + ")********");
+            Console.WriteLine("Gas Price: $" + price);
+            Console.WriteLine("Number of Gallons: " + gallons);
+            Console.WriteLine("Tax Rate: " + taxpercent + "%");
+            Console.WriteLine("Subtotal: $" + subtotal);
+            Console.WriteLine("Tax: $" + tax);
+            Console.WriteLine("Total: $" + total);
+        }
+    }
+}
